76e6a484b75e2d30da0b00a3715e15151455e268
Angus-Leung <angusleung@dhcp-37-62.cu-wireless-faculty.carleton.ca>
2015-01-28 15:14:41 -0500

    Create

---
+++ Display.cc
@@ -0,0 +1,43 @@
+//
+//  Display.cc
+//  
+//
+//  Created by Angus Leung on 2015-01-28.
+//
+//
+
+#include "Display.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Display::mainMenu (int *choice){
+    
+    *choice = -1;
+    
+    cout << "\n\n\n                   WELCOME TO THE BRIG MANAGEMENT SYSTEM \n\n";
+    cout << "( 1 )  Manage pirates\n";
+    cout << "( 2 )  View brig\n";
+    cout << "( 0 )  Exit\n";
+    
+    while (*choice < 0 || *choice > 2) {
+        cout << "Enter your selection:  ";
+        cin >> choice;
+    }
+}
+
+void Display::pirateMenu (){
+    
+    *choice = -1;
+    
+    cout << "\n\n\n                   PIRATE MANAGEMENT MENU \n\n";
+    cout << "( 1 )  Add pirates\n";
+    cout << "( 0 )  Exit\n";
+    
+    while (*choice < 0 || *choice > 1) {
+        cout << "Enter your selection:  ";
+        cin >> choice;
+    }
+
+}
---
+++ Display.h
@@ -0,0 +1,22 @@
+//
+//  menu.h
+//  
+//
+//  Created by Angus Leung on 2015-01-28.
+//
+//
+
+#ifndef   DISPLAY_H
+#define   DISPLAY_H
+#include "defs.h"
+
+Class Display{
+    
+public:
+    //Functions
+    void mainMenu       (int);
+    void pirateMenu     (int);
+    
+}
+
+#endif
