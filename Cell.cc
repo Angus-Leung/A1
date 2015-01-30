97efa8c4f307a84b35bba86b7a96c5c9ed6ba289
Angus-Leung <angusleung@Anguss-MacBook-Pro.local>
2015-01-30 16:31:01 -0500

    Typo fix

--- Cell.cc
+++ Cell.cc
@@ -3,36 +3,18 @@ using namespace std;
 #include "Cell.h"
 
 Cell::Cell() {
-/*	
-	Pirate::Pirate(int i, int s){
-    id = i;
-    space = s;
-}
 
-int getPirateId()
-{
-    return id;
-}
-
-int getPirateSpace(){
-    return space;
-}
-*/
 	
 //Constructor
 
 	Cell:Cell(){
-		cellNumber 		= getCellNum();
+		cellNumber 		= getNextCellNum();
 		spaceRemaining  = MAX_CELL_SPACE;
 		numPirates		= 0;
 	}
-
-//Functions
-/*	int getNextCellNum();
-    int getSpaceRemaining();
-    int getNumPirates();
-    void setSpaceRemaining(); */
 	
+//Functions
+    
 	int Cell::getNextCellNum(){
 		static int id = 1;
 	
