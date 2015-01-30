f1a924bcab8037f029a6759c861ca21e8bc9b0d3
Angus-Leung <angusleung@Anguss-MacBook-Pro.local>
2015-01-30 15:16:35 -0500

    Added PirateArray class

---
+++ PirateArray.cc
@@ -0,0 +1,9 @@
+//
+//  PirateArray.cc
+//  
+//
+//  Created by Angus Leung on 2015-01-30.
+//
+//
+
+#include "PirateArray.h"
---
+++ PirateArray.h
@@ -0,0 +1,14 @@
+//
+//  PirateArray.h
+//  
+//
+//  Created by Angus Leung on 2015-01-30.
+//
+//
+
+#ifndef ____PirateArray__
+#define ____PirateArray__
+
+#include <stdio.h>
+
+#endif /* defined(____PirateArray__) */
