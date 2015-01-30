dfc0ebee9e603ed47f63c2c06f73a58142baba06
lanceencarnado <lance@Lances-MacBook-Air.local>
2015-01-30 15:14:57 -0500

    empty cell header and cc

---
+++ Cell.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#include "Cell.h"
+
+Cell::Cell() {
+    // constructor
+
+}
---
+++ Cell.h
@@ -0,0 +1,19 @@
+#ifndef CELL_H
+#define CELL_H
+
+#include <stdio.h>
+
+class Cell {
+    
+//Data members
+private:
+    
+//Constuctors
+public:
+        
+//Functions
+public:
+
+};
+
+#endif
