61f1edb41c938a6ae3e7f438cefc25167044092d
Angus-Leung <angusleung@Anguss-MacBook-Pro.local>
2015-01-31 18:38:53 -0500

    copied body from main.c to Control.cc

--- Control.cc
+++ Control.cc
@@ -7,3 +7,32 @@
 //
 
 #include "Control.h"
+
+void Display::mainMenu(void)
+{
+    
+    
+    while (choice1 != 0) {
+        mainMenu(&choice1);
+        if (choice1 == 0) {
+            break;
+        }
+        else if (choice1 == 1) {
+            choice2 = -1;
+            while (choice2 != 0) {
+                pirateMenu(&choice2);
+                if (choice2 == 0)
+                    break;
+                else if (choice2 == 1)
+                    addPirates(&brig);
+            }
+        }
+        else if (choice1 == 2) {
+            printBrig(&brig);
+        }
+        if (choice2 != 0)
+            pause();
+    }
+    cleanup(&brig);
+    
+}
