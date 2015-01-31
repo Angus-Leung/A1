6cd3503dbf217bd1e149648aebcc61cc04380a02
lanceencarnado <lance@Lances-MacBook-Air.local>
2015-01-31 18:55:20 -0500

    Cell class: made getNextCellNum a static function

--- Cell.cc
+++ Cell.cc
@@ -1,33 +1,27 @@
 #include <iostream>
 using namespace std;
 #include "Cell.h"
-
-Cell::Cell() {
-
 	
 //Constructor
 
-	Cell:Cell(){
-		cellNumber 		= getNextCellNum();
-		spaceRemaining  = MAX_CELL_SPACE;
-		numPirates		= 0;
-	}
+Cell:Cell() {
+    cellNumber 		= getNextCellNum();
+    spaceRemaining  = MAX_CELL_SPACE;
+    numPirates		= 0;
+}
 	
 //Functions
     
-	int Cell::getNextCellNum(){
-		static int id = 1;
-	
-		return (id++);
-	}
-	
-	int Cell::getSpaceRemaining(){
-		return spaceRemaining;
-	}
-	
-	int Cell::getNumPirates(){
-		return numPirates;
-	}
-	
+static int Cell::getNextCellNum() {
+    static int id = 1;
 
+    return (id++);
+}
+
+int Cell::getSpaceRemaining() {
+    return spaceRemaining;
+}
+
+int Cell::getNumPirates() {
+    return numPirates;
 }
