5202f94e564e3006eafb8d21a9ae87b49144c3a1
Angus-Leung <angusleung@Anguss-MacBook-Pro.local>
2015-01-31 17:32:56 -0500

    Added the control classes (unfinished)

---
+++ Control.cc
@@ -0,0 +1,9 @@
+//
+//  Control.cc
+//  
+//
+//  Created by Angus Leung on 2015-01-31.
+//
+//
+
+#include "Control.h"
---
+++ Control.h
@@ -0,0 +1,14 @@
+//
+//  Control.h
+//  
+//
+//  Created by Angus Leung on 2015-01-31.
+//
+//
+
+#ifndef ____Control__
+#define ____Control__
+
+#include <stdio.h>
+
+#endif /* defined(____Control__) */
